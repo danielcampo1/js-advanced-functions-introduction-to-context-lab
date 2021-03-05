bb7ecffbdb9c86188c5989af291e52769574c114
github username <github email address>
2021-03-05 15:30:20 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,65 @@
-// Your code here
+function createEmployeeRecord(employeeArr) {
+    let newEmployeeObject = {
+        firstName: employeeArr[0],
+        familyName: employeeArr[1],
+        title: employeeArr[2],
+        payPerHour: employeeArr[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return newEmployeeObject
+}
+
+function createEmployeeRecords(arrOfArr) {
+    let employeesArr = arrOfArr.map(createEmployeeRecord)
+    return employeesArr
+}
+
+function createTimeInEvent(employee, dateStamp) {
+    debugger
+    let newTimeIn = {
+        type: "TimeIn",
+        hour: parseInt(dateStamp.split(" ")[1]),
+        date: dateStamp.split(" ")[0]
+    }
+    debugger
+    employee.timeInEvents.push(newTimeIn)
+    debugger
+    return employee
+}
+
+function createTimeOutEvent(employee, dateStamp) {
+    let newTimeOut = {
+        type: "TimeOut",
+        hour: parseInt(dateStamp.split(" ")[1]),
+        date: dateStamp.split(" ")[0]
+    }
+    employee.timeOutEvents.push(newTimeOut)
+    return employee
+}
+
+function hoursWorkedOnDate(employee, date) {
+    let timeOutHour 
+    let timeInHour 
+    for (const timeOut of employee.timeOutEvents) { if (timeOut.date === date) { timeOutHour = timeOut.hour }}
+    for (const timeIn of employee.timeInEvents) { if (timeIn.date === date) { timeInHour = timeIn.hour }}
+    let hoursWorked = (timeOutHour - timeInHour) / 100
+    return hoursWorked
+}
+
+function wagesEarnedOnDate(employee, date) {
+    let wagesEarned = hoursWorkedOnDate(employee, date) * employee.payPerHour
+    return wagesEarned
+}
+
+function allWagesFor(employee) {
+    
+}
+
+function findEmployeeByFirstName(srcArray, firstName) {
+
+}
+
+function calculatePayroll(srcArray) {
+
+}
